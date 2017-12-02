ca24698b9cb413122022c2f5f4e7feaabcf5a153
kellykeller1213 <keller.f.peter@gmail.com>
2017-12-02 20:05:17 +0100

    feat(todo): opens and read the file

--- todo.py
+++ todo.py
@@ -1,17 +1,32 @@
+import sys
+
+todo_file = "todo_list.txt"
+
+
+
 def open_file():
-    pass
+    try:
+        with open(todo_file, 'r') as file:
+            return file.read
+    except FileNotFoundError:
+        print(todo_file + " not found")
+
 
 def save_to_file():
     pass
 
-def print():
+
+def print_data():
     pass
+
 
 def add():
     pass
 
+
 def remove():
     pass
+
 
 def complete():
     pass
